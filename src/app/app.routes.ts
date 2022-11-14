c7ffec16e6528d269af2d27842456d014354a3d3
Bryan <bryanlima200@outlook.com>
2022-11-14 01:30:29 -0300

    Desabilitado Route Tracing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,7 +16,7 @@ const rootRouterConfig: Routes = [
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(rootRouterConfig, { enableTracing: true } )
+        RouterModule.forRoot(rootRouterConfig, { enableTracing: false } )
     ],
     exports: [
         RouterModule
