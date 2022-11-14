5135170e7bc706e18b4f44ff67ff4e2c0301e4f9
Bryan <bryanlima200@outlook.com>
2022-11-14 12:37:16 -0300

    Adicionado módulo Bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
@@ -16,6 +17,7 @@ import { AppComponent } from './app.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
 import { FilmesComponent } from './demos/pipes/filmes/filmes.component';
+import { BarModule } from './demos/bar-di-zones/bar.module';
 import { NavegacaoModule } from './navegacao/navegacao.module';
 
 import { FileSizePipe } from './demos/pipes/filmes/filesize.pipe';
@@ -39,10 +41,12 @@ import { CadastroGuard } from './services/cadastro.guard';
     FormsModule,
     ReactiveFormsModule,
     NavegacaoModule,
+    HttpClientModule,
     TextMaskModule,
     NgBrazil,
     CustomFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BarModule
   ],
   providers: [
     AuthGuard,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,7 @@ import { NotFoundComponent } from "./navegacao/not-found/not-found.component";
 
 import { AuthGuard } from "./services/app.guard";
 import { CadastroGuard } from "./services/cadastro.guard";
+import { BarComponent } from "./demos/bar-di-zones/bar.component";
 
 const rootRouterConfig: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -17,6 +18,7 @@ const rootRouterConfig: Routes = [
     { path: 'cadastro', component: CadastroComponent, canDeactivate: [CadastroGuard] },
     { path: 'produtos', loadChildren: () => import('./demos/arquitetura-componentes/produto.module').then(m => m.ProdutoModule) },
     { path: 'filmes', component: FilmesComponent },
+    { path: 'bar', component: BarComponent },
     {
         path: 'admin', 
         loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
