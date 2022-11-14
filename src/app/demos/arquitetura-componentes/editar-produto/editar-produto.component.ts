1dba8dd0a6094351416ff3394c91b3e9a9fbc0b3
Bryan <bryanlima200@outlook.com>
2022-11-14 01:25:22 -0300

    Navegação imperativa usando Router API

--- src/app/demos/arquitetura-componentes/editar-produto/editar-produto.component.ts
+++ src/app/demos/arquitetura-componentes/editar-produto/editar-produto.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Produto } from '../models/produto';
 import { ProdutoService } from '../services/produto.service';
 
@@ -13,11 +13,21 @@ export class EditarProdutoComponent implements OnInit {
 
   produto: Produto;
 
-  constructor(private route: ActivatedRoute, private produtoService: ProdutoService) { }
+  constructor(
+    private route: ActivatedRoute, 
+    private produtoService: ProdutoService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.produto = this.produtoService.obterPorId(Number(params['id']));
     });
+  }
+
+  salvar() {
+    // Fazer comunicação com backend
+
+    this.router.navigate(['/produtos']);
+    //this.router.navigateByUrl('/produtos');
   }
 }
